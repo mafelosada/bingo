f23a388c26abf453436cdef8642b022d3b2c5846
Maria Fernanda <mafelosi2202@gmail.com>
2024-07-28 17:57:45 -0500

    bingo.js

--- js/bingo.js
+++ js/bingo.js
@@ -1,122 +1,170 @@
 /**
  * Maria Fernanda Losada Silva
  * Bingo
- * 9/07/2024
+ * 23/07/2024
  */
 
-// Generar números aleatorios
-function generarNumerosBingo(cantidad, maximo) {
-    let numeros = new Set();
+let bingo = [];
+let iteracion1;
+let iteracion2;
+let acumulador = 0;
 
-    while (numeros.size < cantidad) {
-        let numeroAleatorio = Math.floor(Math.random() * maximo);
-        numeros.add(numeroAleatorio);
-    }
-    return Array.from(numeros);
+//Generar Bingo
+let tabla = '<table>';
+for (iteracion1 = 0; iteracion1 < 5; iteracion1++) {
+  let interno = [];
+  tabla += '<tr>';  
+  for (iteracion2 = 0; iteracion2 < 5; iteracion2++) {
+    acumulador = acumulador + 1; 
+    interno[iteracion2] = acumulador * 3;
+    tabla += '<td>' + interno[iteracion2] + '</td>'; 
+  }
+  bingo[iteracion1] = interno;
+  tabla += '</tr>'; 
 }
-let numerosBingo1 = generarNumerosBingo(25, 75);
+tabla += '</table>';
+
+document.getElementById('resultado').innerHTML = tabla;  
+
+
+//Letra B
+let tabla2 = '<table>';
+for (iteracion1 = 0; iteracion1 < 5; iteracion1++) {
+  tabla2 += '<tr>';  
+  for (iteracion2 = 0; iteracion2 < 5; iteracion2++) {
+    if (iteracion2 == 0) {
+      tabla2 += '<td class="filaB">' + bingo[iteracion1][iteracion2] + '</td>';
+    }
+    else{
+      tabla2 += '<td>' + bingo[iteracion1][iteracion2] + '</td>'; 
+    }
+  }
+  tabla2 += '</tr>'; 
+}
+tabla2 += '</table>';
+
+document.getElementById('resultado2').innerHTML = tabla2;
+
+
+//Letra I
+let tabla3 = '<table>';
+for (iteracion1 = 0; iteracion1 < 5; iteracion1++) {
+  tabla3 += '<tr>';  
+  for (iteracion2 = 0; iteracion2 < 5; iteracion2++) {
+    if (iteracion2 == 1) {
+      tabla3 += '<td class="filaI">' + bingo[iteracion1][iteracion2] + '</td>';
+    }
+    else{
+      tabla3 += '<td>' + bingo[iteracion1][iteracion2] + '</td>'; 
+    }
+  }
+  tabla3 += '</tr>'; 
+}
+tabla3 += '</table>';
+
+document.getElementById('resultado3').innerHTML = tabla3;
+
+
+//Letra N
+let tabla4 = '<table>';
+for (iteracion1 = 0; iteracion1 < 5; iteracion1++) {
+  tabla4 += '<tr>';  
+  for (iteracion2 = 0; iteracion2 < 5; iteracion2++) {
+    if (iteracion2 == 2) {
+      tabla4 += '<td class="filaN">' + bingo[iteracion1][iteracion2] + '</td>';
+    }
+    else{
+      tabla4 += '<td>' + bingo[iteracion1][iteracion2] + '</td>'; 
+    }
+  }
+  tabla4 += '</tr>'; 
+}
+tabla4 += '</table>';
+
+document.getElementById('resultado4').innerHTML = tabla4;
 
 
 
-// Generar bingo
-function generarBingo(numeros) {
-    let bingo = [];
-    let acumulador = 0;
+//Letra G
+let tabla5 = '<table>';
+for (iteracion1 = 0; iteracion1 < 5; iteracion1++) {
+  tabla5 += '<tr>';  
+  for (iteracion2 = 0; iteracion2 < 5; iteracion2++) {
+    if (iteracion2 == 3) {
+      tabla5 += '<td class="filaG">' + bingo[iteracion1][iteracion2] + '</td>';
+    }
+    else{
+      tabla5 += '<td>' + bingo[iteracion1][iteracion2] + '</td>'; 
+    }
+  }
+  tabla5 += '</tr>'; 
+}
+tabla5 += '</table>';
 
+document.getElementById('resultado5').innerHTML = tabla5;
+
+
+
+//Letra O
+let tabla6 = '<table>';
+for (iteracion1 = 0; iteracion1 < 5; iteracion1++) {
+  tabla6 += '<tr>';  
+  for (iteracion2 = 0; iteracion2 < 5; iteracion2++) {
+    if (iteracion2 == 4) {
+      tabla6 += '<td class="filaO">' + bingo[iteracion1][iteracion2] + '</td>';
+    }
+    else{
+      tabla6 += '<td>' + bingo[iteracion1][iteracion2] + '</td>'; 
+    }
+  }
+  tabla6 += '</tr>'; 
+}
+tabla6 += '</table>';
+
+document.getElementById('resultado6').innerHTML = tabla6;
+
+
+
+//Generar Triple X
+let tabla7 = '<table>';
     for (let iteracion1 = 0; iteracion1 < 5; iteracion1++) {
-        let interno = [];
+        tabla7 += '<tr>';
         for (let iteracion2 = 0; iteracion2 < 5; iteracion2++) {
-            interno[iteracion2] = numeros[acumulador++];
-        }
-        bingo[iteracion1] = interno;
-    }
-    return bingo;
-}
-let bingo1 = generarBingo(numerosBingo1);
-let bingo2 = generarBingo(numerosBingo1);
-let bingo3 = generarBingo(numerosBingo1);
-let bingo4 = generarBingo(numerosBingo1);
-let bingo5 = generarBingo(numerosBingo1);
-let bingo6 = generarBingo(numerosBingo1);
-let bingo7 = generarBingo(numerosBingo1);
-let bingo8 = generarBingo(numerosBingo1);
-
-
-
-// Crear Bingos con colores en las filas
-function crearTabla(bingo, columna1, columna2, columna3, columna4, columna5) {
-    let tabla = '<table>';
-    for (let fila = 0; fila < bingo.length; fila++) {
-        tabla += '<tr>';
-        for (let iteraFila = 0; iteraFila < bingo[fila].length; iteraFila++) {
-            if (columna1 && iteraFila === 0) {
-                tabla += '<td class="filaB">'+bingo2[fila][iteraFila]+'</td>';
-            }
-            else if (columna2 && iteraFila === 1) {
-                tabla += '<td class="filaI">'+bingo3[fila][iteraFila]+'</td>';
-            }
-            else if (columna3 && iteraFila === 2) {
-                tabla += '<td class="filaN">'+bingo4[fila][iteraFila]+'</td>';
-            }
-            else if (columna4 && iteraFila === 3) {
-                tabla += '<td class="filaG">'+bingo5[fila][iteraFila]+'</td>';
-            }
-            else if (columna5 && iteraFila === 4) {
-                tabla += '<td class="filaO">'+bingo6[fila][iteraFila]+'</td>';
-            }
-            else {
-                tabla += '<td>' + bingo1[fila][iteraFila] + '</td>';
-            } 
-        }
-        tabla += '</tr>';
-    }
-    tabla += '</table>';
-    return tabla;
-}
-document.getElementById('resultado').innerHTML = crearTabla(bingo1);
-document.getElementById('resultado2').innerHTML = crearTabla(bingo2, true);
-document.getElementById('resultado3').innerHTML = crearTabla(bingo3, false, true);
-document.getElementById('resultado4').innerHTML = crearTabla(bingo4, false, false, true);
-document.getElementById('resultado5').innerHTML = crearTabla(bingo5, false, false, false, true);
-document.getElementById('resultado6').innerHTML = crearTabla(bingo6, false, false, false, false, true);
-
-
-
-function crearXXX(bingo){
-    let tabla = '<table>';
-    for (let fila = 0; fila < bingo.length; fila++) {
-        tabla += '<tr>';
-        for (let columna = 0; columna < bingo.length; columna++) {
-          if (columna === 0 - fila || fila + columna === 3 - 1 || fila + columna === 4 ||  fila - columna === 3 - 1 || columna === 2 + fila  ) {
-            tabla += '<td class="x">'+bingo7[fila][columna] + " "+'</td>';
+          if (iteracion2 === 0 - iteracion1 || iteracion1 + iteracion2 === 2 || iteracion1 + iteracion2 === 4 ||  iteracion1 - iteracion2 === 2 || iteracion2 === 2 + iteracion1  ) {
+            tabla7 += '<td class="x">'+bingo[iteracion1][iteracion2] + " "+'</td>';
           } else {
-            tabla += '<td>' + bingo1[fila][columna] + '</td>';
+            tabla7 += '<td>' + bingo[iteracion1][iteracion2] + '</td>';
           }
         }
-        tabla += '</tr>';
+        tabla7 += '</tr>';
       }
-      tabla += '</table>';
-      return tabla;
-}
-document.getElementById('resultado7').innerHTML = crearXXX(bingo7);
+      tabla7 += '</table>';
+
+document.getElementById('resultado7').innerHTML = tabla7;
 
 
 
-function crearXGrande(bingo){
-    let tabla = '<table>';
-    for (let fila = 0; fila < bingo.length; fila++) {
-        tabla += '<tr>';
-        for (let columna = 0; columna < bingo.length; columna++) {
-          if (fila === columna || fila + columna === 5 - 1) {
-            tabla += '<td class="x">'+bingo8[fila][columna] + " "+'</td>';
-          } else {
-            tabla += '<td>' + bingo1[fila][columna] + '</td>';
-          }
+//Generar X 
+  let tabla8 = '<table>';
+  for (iteracion1 = 0; iteracion1 < 5; iteracion1++) {
+    tabla8 += '<tr>';
+      for (iteracion2 = 0; iteracion2 < 5; iteracion2++) {
+        if (iteracion1 === iteracion2 || iteracion1 + iteracion2 === 4) {
+          tabla8 += '<td class="x">'+bingo[iteracion1][iteracion2] + " "+'</td>';
+        } else {
+          tabla8 += '<td>' + bingo[iteracion1][iteracion2] + '</td>';
         }
-        tabla += '</tr>';
       }
-      tabla += '</table>';
-      return tabla;
-}  
-document.getElementById('resultado8').innerHTML = crearXGrande(bingo8);
+      tabla8 += '</tr>';
+    }
+    tabla8 += '</table>';
+  
+document.getElementById('resultado8').innerHTML = tabla8;
 
+  
+
+
+
+
+
+ 
